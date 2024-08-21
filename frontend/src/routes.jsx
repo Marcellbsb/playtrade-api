8c3ea8071560d94a8979517fbf3fdb152a27b5e0
covvalente <mateus.mrdo24@gmail.com>
2024-08-21 10:53:41 -0300

    enviando a tela sobre

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,18 +1,21 @@
-import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import LoginPage from './pages/Login';
 import Search from './pages/Search';
-function AppRoutes () {
+import Sobre from './pages/Sobre';
+
+function AppRoutes() {
     return (
-<BrowserRouter>
-<Routes>
-<Route path="/" element = {<Home/>} ></Route>
-<Route path="/register" element = {<Register/>} ></Route>
-<Route path="/search" element = {<Search/>} ></Route>
-<Route path="/login" element = {<LoginPage/>} ></Route>
-</Routes>
-</BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/sobre" element={<Sobre />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
