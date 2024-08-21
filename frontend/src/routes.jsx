b440a3dbe49cbdf6c890bce3fc8837402590714b
Dani <denisedanielasartori@gmail.com>
2024-08-21 19:20:22 -0300

    Tela de Cadastro de Jogos

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import LoginPage from './pages/Login';
 import Search from './pages/Search';
+import GameForm from './pages/GameForm';
 function AppRoutes () {
     return (
 <BrowserRouter>
@@ -11,6 +12,7 @@ function AppRoutes () {
 <Route path="/register" element = {<Register/>} ></Route>
 <Route path="/search" element = {<Search/>} ></Route>
 <Route path="/login" element = {<LoginPage/>} ></Route>
+<Route path="/gameform" element = {<GameForm/>}></Route>
 </Routes>
 </BrowserRouter>
     );
