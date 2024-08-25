f6915fffda44e5b14047ab5a32afed1af811e6cd
LEONARDO SANTOS DA ROCHA <leonardo.santosdarocha@yahoo.com.br>
2024-08-25 15:02:28 -0300

    Update index.jsx

--- frontend/src/pages/Sobre/index.jsx
+++ frontend/src/pages/Sobre/index.jsx
@@ -2,6 +2,7 @@ import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import styles from "./Sobre.module.css";
 
 function Sobre() {
     return (
