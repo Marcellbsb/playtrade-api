ab0cee969038efbf919e83f3ca01503764aee6f2
Dani <denisedanielasartori@gmail.com>
2024-08-25 12:45:30 -0300

    Ajuste CSS tela de Sobre

--- frontend/src/pages/Sobre/index.jsx
+++ frontend/src/pages/Sobre/index.jsx
@@ -9,7 +9,7 @@ function Sobre() {
             <Header />
             <Banner image="mortal" />
             <Container>
-                <p>Sobre o PlayTrade
+                <p id='about'>Sobre o PlayTrade
                     Bem-vindo ao PlayTrade, a plataforma exclusiva para a troca de jogos de mídia física e consoles. Nossa missão é transformar a maneira como você troca e descobre novos jogos, conectando jogadores de todas as regiões do Brasil em um ambiente seguro, intuitivo e sustentável.
                     No PlayTrade, acreditamos que a troca de jogos não deve ser limitada ao seu círculo de conhecidos locais. Nossa plataforma permite que você expanda seus horizontes e encontre jogadores com interesses semelhantes, prontos para compartilhar suas coleções e experiências de jogo. Seja você um jogador casual ou um entusiasta dedicado, o PlayTrade oferece uma solução prática para a troca de jogos e consoles, promovendo a reutilização e a sustentabilidade.
                     Nosso objetivo é facilitar o contato entre jogadores, tornando a busca e a troca de jogos uma experiência agradável e eficiente. Com uma interface amigável e recursos avançados de busca, você pode encontrar rapidamente os jogos que deseja e trocar aqueles que já explorou ao máximo. Além disso, o PlayTrade é um espaço para a troca de informações, dicas e truques, fortalecendo a comunidade gamer e incentivando a interação social.
