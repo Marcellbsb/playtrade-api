7c91eee58e1f3b582e406fe3a7f92cdb326d2aa2
Dani <denisedanielasartori@gmail.com>
2024-08-25 11:46:27 -0300

    Alterações da tela Sobre

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Register';
 import LoginPage from './pages/Login';
 import Search from './pages/Search';
 import GameForm from './pages/GameForm';
+import About from './pages/About';
 function AppRoutes () {
     return (
 <BrowserRouter>
@@ -13,6 +14,7 @@ function AppRoutes () {
 <Route path="/search" element = {<Search/>} ></Route>
 <Route path="/login" element = {<LoginPage/>} ></Route>
 <Route path="/gameform" element = {<GameForm/>}></Route>
+<Route path="/about" element = {<About/>}></Route>
 </Routes>
 </BrowserRouter>
     );
