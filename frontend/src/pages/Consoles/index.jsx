57c37072462f724e6dff229c2431e23aad7fabe1
Dani <denisedanielasartori@gmail.com>
2024-09-08 20:20:54 -0300

    Ajuste tela de consoles.

--- frontend/src/pages/Consoles/index.jsx
+++ frontend/src/pages/Consoles/index.jsx
@@ -1,19 +1,47 @@
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import '../../App.css';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Banner from '../../components/Banner';
-import styles from './Consoles.module.css';
+import Container from '../../components/Container';
+import Card from '../../components/Card';
+import Category from '../../components/Category';
+import axios from 'axios';
 
 function Consoles() {
-   return (
-    <div>
-      <Banner image="viking" />
-      <p>Conheças os consoles disponíveis</p>
-      <Link to="/">
-          <img src="/images/logo2.png" className={styles.logo} alt="logo" />
-      </Link>
-      <p>IMPLEMENTAÇÃO AQUI    **************************</p>
+  const [consoles, setConsoles] = useState([]);
+
+  useEffect(() => {
+    const fetchConsoles = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/consoles');
+        setConsoles(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar consoles:', error);
+      }
+    };
+
+    fetchConsoles();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Banner image="mortal" />
+      <Container>
+        <Category><h2>Consoles</h2></Category>
+        <section className='cards'>
+          {consoles.map((console) => (
+            <Card key={console._id} image_Url={console.image_Url} href={console.href} title={console.title} />
+          ))}
+          {console.length === 0 && (
+            <p>Nenhum console encontrado.</p>
+          )}
+
+        </section>
+      </Container>
       <Footer />
-    </div>
+    </>
   );
 }
 
